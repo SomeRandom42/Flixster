ee63e281646a703ece7016db537f9e3e81e409b8
Patrick Lewis <PatrickDanielLewis@gmail.com>
2017-05-10 20:49:16 +0000

    Added safeguards only enrolled people can view a web page

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,16 @@
 class LessonsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :require_authorized_for_current_lesson, only: [:show]
     def show
     
     end
     
     private
+     def require_authorized_for_current_lesson
+     if current_user.enrolled_in?
+      edirect_to courses_path, alert: 'Error: Not Enrolled in this course.'
+    end
+  end
 
     helper_method :current_lesson
     def current_lesson
